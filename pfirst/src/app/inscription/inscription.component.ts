21ab21262cd52fcbec1e7590fb2a3d51cb4304dd
Bilel S <bilel.saghrouchni@gmail.com>
2020-04-02 16:21:36 +0200

    ajout d'indication pour le mdp

--- pfirst/src/app/inscription/inscription.component.ts
+++ pfirst/src/app/inscription/inscription.component.ts
@@ -12,16 +12,17 @@ import {HttpClient} from '@angular/common/http';
 })
 export class InscriptionComponent implements OnInit {
 
-  current_surname : string;
-  picture_profil_file : File = null;
+  current_surname: string;
+  picture_profil_file: File = null;
   small_password: boolean;
   strong_password: boolean;
   bad_password: boolean;
   medium_password: boolean;
-  badRegex = new RegExp("^[a-zA-Z]{7,}$");
-  mediumRegex = new RegExp("^[a-zA-Z0-9]{8,}$");
-  strongRegex = new RegExp("^.{9,}$");
+  badRegex = new RegExp('^[a-zA-Z]{7,}$');
+  mediumRegex = new RegExp('^[a-zA-Z0-9]{8,}$');
+  strongRegex = new RegExp('^.{9,}$');
   same_password = true;
+  info_mdp = false;
 
 
   constructor(private subService: InscriptionService, private router: Router, private auth: AuthService,
@@ -35,23 +36,23 @@ export class InscriptionComponent implements OnInit {
   onSubmit(form: NgForm) {
     if (this.checkSamePassword(form)) {
       this.same_password = true;
-      console.log("inscription ok");
-      const name = form.value['name'];
-      const prenom = form.value['prenom'];
-      const sexe = form.value['sexe'];
-      const mail = form.value['mail'];
-      const password = form.value['password'];
+      console.log('inscription ok');
+      const name = form.value.name;
+      const prenom = form.value.prenom;
+      const sexe = form.value.sexe;
+      const mail = form.value.mail;
+      const password = form.value.password;
       this.subService.addUser(name, prenom, sexe, mail, password);
 
-      //this.auth.isAuth = true;
+      // this.auth.isAuth = true;
       this.router.navigate(['']);
-    } else if (form.value['password'].length < 6) {
+    } else if (form.value.password.length < 6) {
       form.reset();
-      alert("Le mot de passe doit faire au moins 6 caractères.")
+      alert('Le mot de passe doit faire au moins 6 caractères.');
     } else {
       form.reset();
       this.same_password = false;
-      alert("Les mots de passe entrés ne sont pas identiques.");
+      alert('Les mots de passe entrés ne sont pas identiques.');
 
     }
   }
@@ -61,32 +62,32 @@ export class InscriptionComponent implements OnInit {
   }
 
   checkSamePassword(form: NgForm) {
-    return form.value['password'] === form.value['confirm_password'];
+    return form.value.password === form.value.confirm_password;
   }
-  quickCheckPassword(form: NgForm){
-    if(form.value['password'] != form.value['confirm_password']){
+  quickCheckPassword(form: NgForm) {
+    if (form.value.password !== form.value.confirm_password) {
       this.same_password = false;
-    }else{
+    } else {
       this.same_password = true;
     }
   }
 
   passwordComplexity(text: string) {
 
-    if(text.length < 6){
+    if (text.length < 6) {
       this.small_password = true;
-    }else if( this.badRegex.test(text) ){
+    } else if ( this.badRegex.test(text) ) {
       this.small_password = false;
       this.bad_password = true;
       this.medium_password = false;
       this.strong_password = false;
 
-    }else if( this.mediumRegex.test(text) ){
+    } else if ( this.mediumRegex.test(text) ) {
       this.small_password = false;
       this.medium_password = true;
       this.strong_password = false;
       this.bad_password = false;
-    }else if ( this.strongRegex.test(text) ){
+    } else if ( this.strongRegex.test(text) ) {
       this.small_password = false;
       this.strong_password = true;
       this.bad_password = false;
@@ -95,20 +96,33 @@ export class InscriptionComponent implements OnInit {
 
   }
 
-  onFileSelected(event){
-    this.picture_profil_file = <File>event.target.files[0];
+  onFileSelected(event) {
+    this.picture_profil_file = event.target.files[0] as File;
   }
 
-  onUpload(){
+  onUpload() {
     const fd = new FormData();
-    fd.append('image', this.picture_profil_file, this.picture_profil_file.name)
+    fd.append('image', this.picture_profil_file, this.picture_profil_file.name);
     this.http.post('gs://ineed-1ce51.appspot.com/', fd)
       .subscribe(res => {
         console.log(res);
       });
   }
 
-  updateSurname(text: string){
+  updateSurname(text: string) {
     this.current_surname = text;
+  }
+
+  displayInfo(){
+    this.info_mdp = true;
+    return new Promise(
+      (resolve,reject)=>{
+        setTimeout(()=>{
+            this.info_mdp=false;
+            resolve(true);
+          },5000
+        );
+      }
+    );
   }
 }
