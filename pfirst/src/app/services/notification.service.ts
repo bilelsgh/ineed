e991a825c1dc9f0994c6f986666666e88918c5a1
Thomas D <thomas.dambrin@gmail.com>
2020-05-13 22:01:04 +0200

    structure & fonctions de relation avec firebase ok, forEach error (async function?) #5

--- pfirst/src/app/services/notification.service.ts
+++ pfirst/src/app/services/notification.service.ts
@@ -3,6 +3,7 @@ import {from, Subject} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {Notif} from "../models/notification.model";
 import {NotifierService} from "angular-notifier";
+import {AuthService} from "./auth.service";
 
 @Injectable()
 export class NotificationService {
@@ -16,18 +17,77 @@ export class NotificationService {
   lastNotifIndex: number = 0;
 
   constructor(private httpClient: HttpClient,
-              private notifierService: NotifierService) {
+              private notifierService: NotifierService,
+              private authService: AuthService) {
+    //trigger périodique de la fonction de récupération des notifs
+    let interval = setInterval(() => {
+       this.watchNotifs();
+     }, 5000);
 
-   let interval = setInterval(() => {
-      this.watchNotifs();
-    }, 5000);
+    //installation des notifs firebase
+    this.uploadNotif(new Notif("Notification de test firebase", "warning", "testFb"));
+    this.updateNotifCache(true);
   }
 
-  emitNotifSubject(){
+  uploadNotif(not: Notif) {
+    this.httpClient.put(this.authService.backend_test + 'notifications.json', not)
+      .subscribe(
+        (resp) => {
+          console.log('#Successfully added a new notif : ', not);
+        },
+        (err) => {
+          console.log('#Unable to add a new notif', err);
+        }
+      );
+  }
+
+  updateNotifCache(toCheck: boolean) {
+    this.httpClient.put(this.authService.backend_test + 'cacheNotif.json', toCheck)
+      .subscribe(
+        (resp) => {
+          console.log('#Successfully set the cache', resp);
+        },
+        (err) => {
+          console.log('#Unable to set the cache', err);
+        }
+      );
+  }
+
+  getNoticationFromBack() {
+    return new Promise((resolve, reject) => {
+      this.httpClient.get<Notif[]>(this.authService.backend_test + 'notifications.json')
+        .subscribe(
+          (got) => {
+            this.notifList = got;
+            resolve('Got the notifications');
+          },
+          (err) => {
+            reject('Cannot get the notifications');
+          }
+        );
+    });
+  }
+
+  checkNotifCache() {
+    return new Promise(((resolve, reject) => {
+      this.httpClient.get<boolean>(this.authService.backend_test + 'cacheNotif.json')
+        .subscribe(
+          (result) => {
+            this.hasNew = result;
+            resolve('Checked the cache');
+          },
+          (err) => {
+            reject('Unable to check the cache');
+          }
+        );
+    }));
+  }
+
+  emitNotifSubject() {
     this.notifSubject.next(this.notifList);
   }
 
-  triggerNotif(not: Notif){
+  triggerNotif(not: Notif) {
     this.notifierService.show({
       type: not.type,
       message: not.message,
@@ -36,23 +96,35 @@ export class NotificationService {
     this.alreadyNotified.push(not);
   }
 
-  addNotif(not: Notif){
+  addNotif(not: Notif) {
     this.notifList.push(not);
     this.emitNotifSubject();
   }
 
-  watchNotifs(){
-    if (this.hasNew){
-      this.addNotif(new Notif('Chargez une photo de profil', 'info', 'profilPic'));
-      this.hasNew = false;
-      this.notifList.forEach((elt)=>{
-        if (!this.alreadyNotified.includes(elt)){
-          this.triggerNotif(elt);
+  watchNotifs() {
+    this.checkNotifCache()
+      .then((msg) => {
+        console.log(msg);
+        if (this.hasNew) {
+          //this.addNotif(new Notif('Chargez une photo de profil', 'info', 'profilPic'));
+          this.hasNew = false;
+          this.getNoticationFromBack()
+            .then((secondMsg) => {
+              this.notifList.forEach((elt) => {
+                if (!this.alreadyNotified.includes(elt)) {
+                  this.triggerNotif(elt);
+                }
+              });
+              this.alreadyNotified = this.notifList;
+              console.log(secondMsg);
+            })
+            .catch((secondMsg) => {
+              console.log(secondMsg);
+            });
         }
+      })
+      .catch((msg) => {
+        console.log(msg);
       });
-    }else{
-      this.hasNew = false;
-      //this.triggerNotif(this.notifList[this.lastNotifIndex]);
-    }
   }
 }
