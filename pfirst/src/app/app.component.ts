5338b63991ed639cf7858acd668a09e6075ea498
Bilel S <bilel.saghrouchni@gmail.com>
2020-05-17 12:12:54 +0200

    Ajout de ngAfterViewInit résolution partielle du problème

--- pfirst/src/app/app.component.ts
+++ pfirst/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit} from '@angular/core';
 import {AuthService} from "./services/auth.service";
 import {UserService} from './services/users.service';
 import {Subscription} from 'rxjs';
@@ -8,16 +8,32 @@ import {Subscription} from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit, AfterViewInit{
   dark_theme : boolean;
   light_theme : boolean;
   light_theme_sub : Subscription;
   dark_theme_sub : Subscription;
+  title = 'INEED';
 
-  constructor(private userServ : UserService) {
+  constructor(private userServ : UserService, private elementRef: ElementRef) {
   }
 
   ngOnInit(){
+    this.setTheme();
+  }
+
+  ngAfterViewInit() {
+    this.setTheme();
+    if(this.dark_theme && !this.light_theme){
+      this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#2e2e2e';
+    }else if(!this.dark_theme && this.light_theme){
+      this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = 'whitesmoke';
+    }
+  }
+
+
+  setTheme(){
+
     //Récupération de l'information permettant d'appliquer une theme sombre ou clair
     this.dark_theme_sub = this.userServ.darkThemeSubject.subscribe(
       (response: boolean) => {
@@ -37,5 +53,5 @@ export class AppComponent implements OnInit{
     this.userServ.emitDarkThemeSubject();
 
   }
-  title = 'INEED';
 }
+
