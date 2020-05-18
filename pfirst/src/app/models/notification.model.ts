edaf7801a78f4e72ad7944f770302fd15a04fab1
Thomas D <thomas.dambrin@gmail.com>
2020-05-18 15:50:39 +0200

    ajout champs notif #5

--- pfirst/src/app/models/notification.model.ts
+++ pfirst/src/app/models/notification.model.ts
@@ -1,6 +1,7 @@
 export class Notif{
   constructor(public message: string,
               public type: string,
-              public id: string) {
+              public id: string,
+              public category: string = "profil") {
   }
 }
