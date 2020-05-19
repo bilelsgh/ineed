d41c1733c13fb58640348a03f1f873b57fcbecc8
Thomas D <thomas.dambrin@gmail.com>
2020-05-19 16:43:16 +0200

    send review ok #19

--- pfirst/src/app/review/review.component.ts
+++ pfirst/src/app/review/review.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {ActivatedRoute} from "@angular/router";
+import {NgForm} from "@angular/forms";
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-review',
@@ -10,10 +12,33 @@ import {ActivatedRoute} from "@angular/router";
 export class ReviewComponent implements OnInit {
 
   announceId: string;
+  rate: number = 0;
   constructor(private httpClient: HttpClient,
-              private route: ActivatedRoute) {}
+              private route: ActivatedRoute,
+              private authService: AuthService) {}
   ngOnInit(): void {
     this.announceId = this.route.snapshot.params['id'];
+  }
+
+  onSubmitReview(form: NgForm) {
+    let comment: string = form.value.comment;
+    this.httpClient.post(this.authService.backend + 'api/review', {
+      'token': JSON.parse(localStorage.getItem('token')),
+      'review': {
+        'announce': this.announceId,
+        'content': comment,
+        'author': JSON.parse(localStorage.getItem('user')).idUser,
+        'note': this.rate
+      }
+    }).subscribe(
+      (resp) => {
+        console.log("SUCCESS REVIEW",resp);
+        form.reset();
+      },
+      (e) => {
+        console.log("FAILURE REVIEW",e);
+      }
+    );
   }
 
 }
