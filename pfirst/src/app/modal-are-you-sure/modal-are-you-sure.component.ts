7a8ef3d7203f62cb6cbbe902f8068eb3081f27de
Bilel S <bilel.saghrouchni@gmail.com>
2020-05-23 19:10:58 +0200

    fonction pour delete une annonce, à tester #4

--- pfirst/src/app/modal-are-you-sure/modal-are-you-sure.component.ts
+++ pfirst/src/app/modal-are-you-sure/modal-are-you-sure.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {SuiviService} from '../services/suivi.service';
+import {HttpClient} from '@angular/common/http';
+import {AuthComponent} from '../auth/auth.component';
+import {AuthService} from '../services/auth.service';
 
 @Component({
   selector: 'app-modal-are-you-sure',
@@ -8,9 +11,29 @@ import {SuiviService} from '../services/suivi.service';
 })
 export class ModalAreYouSureComponent implements OnInit {
 
-  constructor(public suiviServ : SuiviService) { }
+  constructor(public suiviServ : SuiviService, public httpClient : HttpClient, public auth : AuthService) { }
+
+  public idAnnounce; //id de l'annonce à supprimer
 
   ngOnInit(): void {
+    this.idAnnounce = this.suiviServ.serviceId_selected;
   }
 
-}
+  deleteAnnounce(){
+    this.httpClient.delete(this.auth.backend + "api/announce/" + this.idAnnounce)
+      .subscribe(
+        (response) => {
+          console.log("suppression ok");
+          console.table(response);
+        },
+        (error) => {
+          if (error['status'] === 401) {
+            this.auth.removeUserInfo();
+            console.log('#TOKEN EXPIRED');
+          }
+        }
+      )
+  }
+
+  }
+
