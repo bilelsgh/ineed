4436f6a65db20b3efc2981aa902307332aaeb358
Thomas D <thomas.dambrin@gmail.com>
2020-05-24 11:28:10 +0200

    review de tous les participants si auteur, auteur si un participant #19

--- pfirst/src/app/review/review.component.ts
+++ pfirst/src/app/review/review.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {ActivatedRoute} from '@angular/router';
 import {NgForm} from '@angular/forms';
 import {AuthService} from '../services/auth.service';
+import {SuiviService} from "../services/suivi.service";
 
 @Component({
   selector: 'app-review',
@@ -13,19 +14,70 @@ export class ReviewComponent implements OnInit {
 
   announceId: string;
   rate = 0;
-  successfullySended: boolean;
-  ratedName = '';
+  successfullySended = {};
+  ratedNames: string[] = new Array();
+  announceAuthor: boolean;
+  participants: number[] = new Array();
+  idToName = {};
 
   constructor(private httpClient: HttpClient,
               private route: ActivatedRoute,
-              private authService: AuthService) {}
+              private authService: AuthService,
+              private suiviService: SuiviService) {
+  }
+
   ngOnInit(): void {
     this.successfullySended = false;
     this.announceId = this.route.snapshot.params.idAnnounce;
-    this.ratedName = this.route.snapshot.params.ratedName;
+    this.getAuthor(this.announceId)
+      .then( () => {
+          if (!this.announceAuthor){
+            this.suiviService.getAssignees(this.announceId)
+              .then( () => {
+                this.participants = this.suiviService.assignees;
+                this.participants.forEach( (part) => {
+                  this.getName(part);
+                });
+              }).catch( (e) => {
+              console.log('Erreur getAssignees', e);
+            });
+          }else{
+            this.getName(this.participants[0]);
+          }
+      })
+      .catch( (e) => {
+        console.log(e);
+      });
+    //this.ratedName = this.route.snapshot.params.ratedName;
   }
 
-  onSubmitReview(form: NgForm) {
+  getName(idUsr: number){
+    this.httpClient.get(this.authService.backend + 'api/user/' + idUsr + '?token=' + JSON.parse(localStorage.getItem('token')))
+      .subscribe( (resp) => {
+        this.idToName[idUsr] = resp['user'].firstName;
+        this.successfullySended[idUsr] = false;
+        this.authService.setUserInfo(JSON.stringify(resp['token']), 'token');
+      });
+  }
+
+  getAuthor(annId: number) {
+    return new Promise((resolve, reject) => {
+      this.httpClient.get(this.authService.backend + 'api/announce/' + annId + "?token=" + JSON.parse(localStorage.getItem('token')))
+        .subscribe((resp) => {
+            this.announceAuthor = JSON.parse(localStorage.getItem('user')).idUser == resp['announce'].idUser;
+            if (this.announceAuthor) {
+              this.participants.push(resp['announce'].idUser);
+            }
+            this.authService.setUserInfo(JSON.stringify(resp['token']), 'token');
+            resolve(true);
+          },
+          (e) => {
+            resolve(e);
+          });
+    });
+  }
+
+  onSubmitReview(form: NgForm, idRated: number) {
     const comment: string = form.value.comment;
     this.httpClient.post(this.authService.backend + 'api/review', {
       token: JSON.parse(localStorage.getItem('token')),
@@ -39,7 +91,7 @@ export class ReviewComponent implements OnInit {
       (resp) => {
         console.log('SUCCESS REVIEW', resp);
         form.reset();
-        this.successfullySended = true;
+        this.successfullySended[idRated] = true;
       },
       (e) => {
         console.log('FAILURE REVIEW', e);
